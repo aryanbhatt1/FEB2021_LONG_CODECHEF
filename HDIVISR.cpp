2836872d9c92cdcf01a7241cf0b6a0d905d80ba5
AryanBhatt <aryanbhatt1002@gmail.com>
2021-02-05 18:03:56 +0530

    commit

---
+++ HDIVISR.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int number;
+    vector<int> arr;
+    cin>>number;
+    for(int i =1; i<=number; i++){
+        if(number%i==0 & i>0 &i<=10){
+            arr.push_back(i);
+        }
+    }
+    cout<<*max_element(arr.begin(), arr.end())<<endl;
+    return 0;
+}
